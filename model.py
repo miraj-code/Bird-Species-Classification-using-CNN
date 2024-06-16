bed086b321fc776d4b0a52fb9ba69df596959d86
Leul Getaneh <60969133+miraj-code@users.noreply.github.com>
2024-06-16 12:22:32 +0300

    Update model.py
    - Added a comment to explain the code snippet

--- model.py
+++ model.py
@@ -1,7 +1,10 @@
+# Import the necessary libraries
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+# Create the model using Convolutional Neural Network
 class ConvolutionalNeuralModel(nn.Module):
+    # Initialize the class    
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
