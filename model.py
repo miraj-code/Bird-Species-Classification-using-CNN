04c73792a8b0e4c6349186b6066d3fd38911ecbc
Leul Getaneh <60969133+miraj-code@users.noreply.github.com>
2024-06-17 18:27:45 +0300

    Update model.py
    - Added comment to explain the code snippet

--- model.py
+++ model.py
@@ -5,20 +5,23 @@ import torch.nn.functional as F
 
 # Create the model using Convolutional Neural Network
 class ConvolutionalNeuralModel(nn.Module):
-    # Initialize the class    
+    # Initialize the model class    
     def __init__(self) -> None:
         super().__init__()
+        # Create two convolution layers
         self.conv1 = nn.Conv2d(1, 6, 3, 1)
         self.conv2 = nn.Conv2d(6, 16, 3, 1)
 
+        # Create three fully connected Linear layer
         self.fc1 = nn.Linear(5*5*16, 1200)
         self.fc2 = nn.Linear(1200, 800)
         self.fc3 = nn.Linear(800, 525)
         
+    # A function that forward the data through different layers
     def forward(self, X):
+        # Create four relu and two max_pool2d layers 
         X = F.relu(self.conv1(X))
         X = F.max_pool2d(X, 2, 2)
-
         X = F.relu(self.conv1(X))
         X = F.max_pool2d(X, 2, 2)
 
@@ -29,6 +32,8 @@ class ConvolutionalNeuralModel(nn.Module):
         X = self.fc3(X)
 
         return F.log_softmax(X, dim=1)
-    
+
+# Set the manual seed
 torch.manual_seed(41)
+# initialize the model
 model = ConvolutionalNeuralModel()
